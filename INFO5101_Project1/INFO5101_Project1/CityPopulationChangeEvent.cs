8f5ac15c7f075aa5486c553e4b2c1b6e9e570d27
unknown <benvgaming@pm.me>
2022-02-12 15:49:24 -0500

    Added 3 classes

---
+++ INFO5101_Project1/INFO5101_Project1/CityPopulationChangeEvent.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace INFO5101_Project1
+{
+    public class CityPopulationChangeEvent
+    {
+        //This is an optional class
+        //We can do it if you guys want ONE bonus mark
+    }
+}
