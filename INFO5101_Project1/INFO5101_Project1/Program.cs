8242876ddc7724b44648a5f6f85b53889e7faaeb
unknown <benvgaming@pm.me>
2022-02-12 15:15:53 -0500

    Initial files

---
+++ INFO5101_Project1/INFO5101_Project1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace INFO5101_Project1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
